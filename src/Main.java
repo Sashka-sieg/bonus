f449a333e166c9a6a1b6961070a4259cc7053bbd
Yakushin Alexandr <valensiec08@mail.ru>
2021-10-06 22:47:59 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int customerAccount = 500;
+        int replenishmentAmount = 3000;
+        int bonus = 0;
+        int bonusAmount = 1000;
+        int balance = 0;
+        if (replenishmentAmount >= bonusAmount) {
+            bonus = replenishmentAmount / 100;
+            balance = customerAccount +replenishmentAmount + bonus;
+        }
+            System.out.println(bonus);
+            System.out.println(balance);
+    }
+
+    }

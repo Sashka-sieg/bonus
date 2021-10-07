d27b7e6eb0a81499111ca22f588b337269672fa2
Sashka-sieg <89997116+Sashka-sieg@users.noreply.github.com>
2021-10-07 20:39:35 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int customerAccount = 500;
-        int replenishmentAmount = 3000;
+        int replenishmentAmount = 1199;
         int bonus = 0;
         int bonusAmount = 1000;
-        int overallBalance = 0;
-        if (replenishmentAmount >= bonusAmount) {
+        if (replenishmentAmount > bonusAmount) {
             bonus = replenishmentAmount / 100;
-            overallBalance = customerAccount +replenishmentAmount + bonus;
         }
-            System.out.println("Бонус начислений:" + bonus + "руб");
-            System.out.println("Баланс лицевого счета:" + overallBalance + "руб");
+        System.out.println("Бонус начислений:" + bonus + "руб");
+        System.out.println(customerAccount+replenishmentAmount+bonus );
     }
 
-    }
+}
